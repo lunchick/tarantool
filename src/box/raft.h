bca27b4587366a413e0be506552a1abc2c836f34
Cyrill Gorcunov <gorcunov@gmail.com>
2020-11-06 11:31:14 +0300

    raft: use vclock_map_t for voting tracking
    We've VCLOCK_MASK constant which limits the number of
    regular nodes in a replication cluster. This limit is
    bound to the vclock_map_t bitset type. Thus when we're
    tacking voting process in Raft node we better use this
    type for vote_mask member (otherwise it is a room for
    error if we ever change VCLOCK_MASK and extend the width
    of a bitset).
    
    Suggested-by: Vladislav Shpilevoy <v.shpilevoy@tarantool.org>
    Signed-off-by: Cyrill Gorcunov <gorcunov@gmail.com>

--- src/box/raft.h
+++ src/box/raft.h
@@ -33,6 +33,7 @@
 #include <stdbool.h>
 #include "tarantool_ev.h"
 #include "trigger.h"
+#include "vclock.h"
 
 #if defined(__cplusplus)
 extern "C" {
@@ -146,7 +147,7 @@ struct raft {
 	 * Bit 1 on position N means that a vote from instance with ID = N was
 	 * obtained.
 	 */
-	uint32_t vote_mask;
+	vclock_map_t vote_mask;
 	/** Number of votes for this instance. Valid only in candidate state. */
 	int vote_count;
 	/** State machine timed event trigger. */
